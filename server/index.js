b8281d908a19dab0c25c8e0cc7d217053244dddd
andrew hunsicker <ahunsicker888@gmail.com>
2022-04-27 17:45:28 -0400

    testing css

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ const path = require('path')
 
 const app = express()
 
-app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/main.js'))
-  })
+
+app.use(express.static("public"))
+// app.get('/js', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/main.js'))
+//   })
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
